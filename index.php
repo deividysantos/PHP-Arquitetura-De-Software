e6afa31bdee66b74f5ff67d626c91bd91742846c
felipeverse <felipealvesrrodrigues@outlook.com>
2022-02-20 17:38:29 -0400

    improve

--- index.php
+++ index.php
@@ -2,8 +2,6 @@
 
     session_start();
 
-    dd($_POST);
-
     $host = 'controle-financeiro-mysql';
     $db = 'controle-financeiro-database';
     $user = 'root';
@@ -29,32 +27,20 @@
                     $lancamento['data_lancamento'] = $_POST['data_lancamento'];
                     $lancamento['operacao'] = $_POST['operacao'];
 
-                    $sql = 'INSERT INTO lancamentos (titulo, descricao, valor, data_lancamento, operacao) VALUES (?, ?, ?, ?, ?)';
-
-                    $statement = $conexao->prepare($sql);
-                    
-                    $statement->bindValue(1, $lancamento['titulo']);
-                    $statement->bindValue(2, $lancamento['descricao']);
-                    $statement->bindValue(3, $lancamento['valor']);
-                    $statement->bindValue(4, $lancamento['data_lancamento']);
-                    $statement->bindValue(5, $lancamento['operacao']);
-
-
-                    if ($statement->execute()) 
+                    if(insertLancamento($lancamento))
                     {
                         setMessage('success', 'Inserido com sucesso.');
-                        header('Location: index.php');
-                    }
+                    } 
                     else 
                     {
                         setMessage('error', 'Erro na inserção.');
                     }
-
                 } else
                 {
                     setMessage('error', 'Dados inseridos inválidos.');
                 }
                 header('Location: index.php');
+                die();
                 break;
             
             case 'excluir' :
@@ -62,25 +48,19 @@
                 {
                     $id = $_POST['id'];
 
-                    $sql = "DELETE FROM lancamentos WHERE id = ?";
-                    
-                    $statement = $conexao->prepare($sql);
-
-                    $statement->bindValue(1, $id);
-
-                    if ($statement->execute())
+                    if (deletarLancamento($id))
                     {
                         setMessage('success', 'Excluido com sucesso.');
-                        header('Location: index.php');
                     }
                     else
                     {
                         setMessage('error', 'Erro na exclusão.');
-                        header('Location: index.php');
                     }
                 }
+                header('Location: index.php');
+                die();
                 break;
-
+                
             case 'editar' :
                 if(!empty($_POST['id'])) 
                 {
@@ -88,18 +68,8 @@
                     $emEdicao = true;
                     $id = $_POST['id'];
 
-                    $sql = "SELECT * FROM lancamentos WHERE id = ?";
-
-                    $statement = $conexao->prepare($sql);
-
-                    $statement->bindValue(1, $id);
-
-                    if ($statement->execute())
-                    {
-                        $lancamento = $statement->fetch(PDO::FETCH_ASSOC);
-                    } 
+                    $lancamento = getLancamento($id);
                 }
-
                 break;
                 
             case 'atualizar' :
@@ -109,32 +79,22 @@
                     $lancamento = [];
                     $lancamento['titulo'] = $_POST['titulo'];
                     $lancamento['descricao'] = $_POST['descricao'];
+                    $lancamento['data_lancamento'] = $_POST['data_lancamento'];
                     $lancamento['valor'] = $_POST['valor'];
                     $lancamento['operacao'] = $_POST['operacao'];
 
-                    $sql = "UPDATE lancamentos SET titulo = ?, descricao = ?, valor = ?, operacao = ? WHERE id = ?";
-
-                    $statement = $conexao->prepare($sql);
-
-                    $statement->bindValue(1, $lancamento['titulo']);
-                    $statement->bindValue(2, $lancamento['descricao']);
-                    $statement->bindValue(3, $lancamento['valor']);
-                    $statement->bindValue(4, $lancamento['operacao']);
-                    $statement->bindValue(5, $id);
-
-                    if ($statement->execute())
+                    if (updateLancamento($id, $lancamento))
                     {
                         setMessage('success', 'Atualizado com sucesso.');
-                        header('Location: index.php');
                     } else
                     {
                         setMessage('error', 'Erro ao atualizar.');
-                        header('Location: index.php');
                     }
                 }
+                header('Location: index.php');
+                die();
                 break;
         }
-
     }
 
     function validarLancamento() 
@@ -190,6 +150,96 @@
         return [];
     }
 
+    function insertLancamento($lancamento)
+    {
+        global $conexao;
+        
+        $sql = 'INSERT INTO lancamentos (titulo, descricao, valor, data_lancamento, operacao) VALUES (?, ?, ?, ?, ?)';
+        
+        $statement = $conexao->prepare($sql);
+
+        $statement->bindValue(1, $lancamento['titulo']);
+        $statement->bindValue(2, $lancamento['descricao']);
+        $statement->bindValue(3, $lancamento['valor']);
+        $statement->bindValue(4, $lancamento['data_lancamento']);
+        $statement->bindValue(5, $lancamento['operacao']);
+
+        if ($statement->execute()) 
+        {
+            return true;
+        }
+        else 
+        {
+            return false;
+        }
+    }
+
+    function deletarLancamento($id)
+    {
+        global $conexao;
+        
+        $sql = "DELETE FROM lancamentos WHERE id = ?";
+
+        $statement = $conexao->prepare($sql);
+
+        $statement->bindValue(1, $id);
+
+        if ($statement->execute())
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    function getLancamento($id)
+    {
+        global $conexao;
+
+        $sql = "SELECT * FROM lancamentos WHERE id = ?";
+
+        $statement = $conexao->prepare($sql);
+
+        $statement->bindValue(1, $id);
+
+        if ($statement->execute()) 
+        {
+            $lancamento = $statement->fetch(PDO::FETCH_ASSOC);
+            return $lancamento;
+        } else
+        {
+            return false;
+        }
+
+    }
+
+    function updateLancamento($id, $lancamento)
+    {
+        global $conexao;
+
+        $sql = "UPDATE lancamentos SET titulo = ?, descricao = ?, data_lancamento = ?, valor = ?, operacao = ? WHERE id = ?";
+        
+        $statement = $conexao->prepare($sql);
+
+        $statement->bindValue(1, $lancamento['titulo']);
+        $statement->bindValue(2, $lancamento['descricao']);
+        $statement->bindValue(3, $lancamento['data_lancamento']);
+        $statement->bindValue(4, $lancamento['valor']);
+        $statement->bindValue(5, $lancamento['operacao']);
+        $statement->bindValue(6, $id);
+
+        if ($statement->execute())
+        {
+            return true;
+        } else
+        {
+            return false;
+        }
+
+    }
+
     function dd($var) 
     {
         echo "<pre>";
@@ -221,16 +271,17 @@
 <body class="bg-indigo-300 p-4">
     <?php
         $message = getMessage();
-        if($message) {
+        if ($message) {
             echo "<pre>";
             var_dump($message);
             echo "</pre>";
             unsetMessage();
         }
+
     ?>
     <div>
         <form action="index.php" method="POST">
-            <h2 class="min-w-min text-center text-xl">Novo ançamento</h2>
+            <h2 class="min-w-min text-center text-xl">Novo Lançamento</h2>
 
             <div class="max-w-min mx-auto mt-8">
                 <div>
@@ -247,7 +298,7 @@
                 </div>
                 <div>
                     <label for="data_lancamento">Data Lançamento: </label>
-                    <input type="date" name="data_lancamento" value="<?= date('Y-m-d') ?>" required>
+                    <input type="date" name="data_lancamento" value="<?= isset($lancamento['data_lancamento'])? $lancamento['data_lancamento'] : date('Y-m-d') ?>" required>
                 </div>
                 <div>
                     <label for="operacao">Valor: </label>
@@ -274,39 +325,41 @@
     </div>
     <?php  
             $lancamentos = getLancamentos();
-            if(count($lancamentos) > 0) {
-                echo "<pre>";
-                var_dump($lancamentos);
-                echo "</pre>";
-
-                echo "<div>";
-                echo "<table>";
-                foreach ($lancamentos as $lancamento): ?>
+            if(count($lancamentos) > 0): ?>
+            <div>
+                <table>
                     <tr>
-                        <td><?= $lancamento['id'] ?></td>
-                        <td><?= $lancamento['titulo']?></td>
-                        <td><?= $lancamento['descricao']?></td>
-                        <td><?= $lancamento['valor']?></td>
-                        <td><?= $lancamento['operacao']?></td>
-                        <td>
-                            <form action="index.php" method="POST">
-                                <input type="hidden" name="id" value="<?= $lancamento['id'] ?>">
-                                <input type="submit" value="excluir" name="action">
-                            </form>
-                        </td>
-                        <td>
-                            <form action="index.php" method="POST">
-                                <input type="hidden" name="id" value="<?= $lancamento['id'] ?>">
-                                <input type="submit" value="editar" name="action">
-                            </form>
-                        </td>
-                        
-                    </tr>      
-                <?php endforeach;
-                echo "<table>";
-                echo "</div>";
-            }
-        ?>
+                        <th># ID</th>
+                        <th>Título</th>
+                        <th>Valor</th>
+                        <th>Data Lanc.</th>
+                        <th>Operacao</th>
+                    </tr>
+                    <?php foreach ($lancamentos as $lancamento): ?>
+                        <tr>
+                            <td><?= $lancamento['id'] ?></td>
+                            <td><?= $lancamento['titulo']?></td>
+                            <td><?= $lancamento['valor']?></td>
+                            <td><?= DateTime::createFromFormat('Y-m-d', $lancamento['data_lancamento'])->format('d/m/Y') ?></td>
+                            <td><?= $lancamento['operacao']?></td>
+                            <td>
+                                <form action="index.php" method="POST">
+                                    <input type="hidden" name="id" value="<?= $lancamento['id'] ?>">
+                                    <input type="submit" value="excluir" name="action">
+                                </form>
+                            </td>
+                            <td>
+                                <form action="index.php" method="POST">
+                                    <input type="hidden" name="id" value="<?= $lancamento['id'] ?>">
+                                    <input type="submit" value="editar" name="action">
+                                </form>
+                            </td>
+                            
+                        </tr>      
+                    <?php endforeach; ?>
+                <table>
+            </div>
+            <?php endif; ?>
     </div>
 </body>
 </html>
