53fdc63dc3e94c8d4a20b81efce01d2efeb9d971
felipeverse <felipealvesrrodrigues@outlook.com>
2022-02-19 20:44:59 -0400

    Implementa codigo procedural das operacoes

--- index.php
+++ index.php
@@ -1,206 +1,282 @@
 <?php
 
-$error   = false;
-$success = false;
-$message = '';
+    session_start();
 
-if (!empty($_POST)) {
+    $host = 'database';
+    $db = 'app-controle-financeiro';
+    $user = 'root';
+    $pass = 'root';
 
-    if ($_POST['action'] == 'inserir') {
-        $titulo    = $_POST['titulo'];;
-        $concluida = $_POST['concluida'];
+    $dsn = "mysql:host=$host;dbname=$db";
+    $conexao = new PDO($dsn, $user,$pass);
 
-        if (empty($titulo)) {
-            $error   = true;
-            $message = "Dados inseridos inválidos.";
+    $emEdicao = false;
+
+    if (!empty($_POST)) 
+    {
+
+        switch ($_POST['action']) 
+        {
+            case 'inserir' : 
+                if(validarLancamento()) 
+                {
+                    $lancamento = [];
+                    $lancamento['nome'] = $_POST['nome'];
+                    $lancamento['tipo'] = $_POST['tipo'];
+                    $lancamento['valor'] = $_POST['valor'];
+                    $lancamento['operacao'] = $_POST['operacao'];
+
+                    $sql = 'INSERT INTO lancamentos (nome, tipo, valor, operacao) VALUES (?, ?, ?, ?)';
+
+                    $statement = $conexao->prepare($sql);
+                    
+                    $statement->bindValue(1, $lancamento['nome']);
+                    $statement->bindValue(2, $lancamento['tipo']);
+                    $statement->bindValue(3, $lancamento['valor']);
+                    $statement->bindValue(4, $lancamento['operacao']);
+
+                    if ($statement->execute()) 
+                    {
+                        setMessage('success', 'Inserido com sucesso.');
+                        header('Location: index.php');
+                    }
+                    else 
+                    {
+                        setMessage('error', 'Erro na inserção.');
+                    }
+
+                } else
+                {
+                    setMessage('error', 'Dados inseridos inválidos.');
+                }
+                header('Location: index.php');
+                break;
+            
+            case 'excluir' :
+                if(!empty($_POST['id'])) 
+                {
+                    $id = $_POST['id'];
+
+                    $sql = "DELETE FROM lancamentos WHERE id = ?";
+                    
+                    $statement = $conexao->prepare($sql);
+
+                    $statement->bindValue(1, $id);
+
+                    if ($statement->execute())
+                    {
+                        setMessage('success', 'Excluido com sucesso.');
+                        header('Location: index.php');
+                    }
+                    else
+                    {
+                        setMessage('error', 'Erro na exclusão.');
+                        header('Location: index.php');
+                    }
+                }
+                break;
+
+            case 'editar' :
+                if(!empty($_POST['id'])) 
+                {
+
+                    $emEdicao = true;
+                    $id = $_POST['id'];
+
+                    $sql = "SELECT * FROM lancamentos WHERE id = ?";
+
+                    $statement = $conexao->prepare($sql);
+
+                    $statement->bindValue(1, $id);
+
+                    if ($statement->execute())
+                    {
+                        $lancamento = $statement->fetch(PDO::FETCH_ASSOC);
+                    } 
+                }
+
+                break;
+                
+            case 'atualizar' :
+                if (!empty($_POST['id']))
+                {
+                    $id = $_POST['id'];
+                    $lancamento = [];
+                    $lancamento['nome'] = $_POST['nome'];
+                    $lancamento['tipo'] = $_POST['tipo'];
+                    $lancamento['valor'] = $_POST['valor'];
+                    $lancamento['operacao'] = $_POST['operacao'];
+
+                    $sql = "UPDATE lancamentos SET nome = ?, tipo = ?, valor = ?, operacao = ? WHERE id = ?";
+
+                    $statement = $conexao->prepare($sql);
+
+                    $statement->bindValue(1, $lancamento['nome']);
+                    $statement->bindValue(2, $lancamento['tipo']);
+                    $statement->bindValue(3, $lancamento['valor']);
+                    $statement->bindValue(4, $lancamento['operacao']);
+                    $statement->bindValue(5, $id);
+
+                    if ($statement->execute())
+                    {
+                        setMessage('success', 'Atualizado com sucesso.');
+                        header('Location: index.php');
+                    } else
+                    {
+                        setMessage('error', 'Erro ao atualizar.');
+                        header('Location: index.php');
+                    }
+                }
+                break;
         }
 
-        if (empty($concluida)) {
-            $error   = true;
-            $message = "Dados inseridos inválidos.";
-        }
-
-        if (!$error) {
-            $link = mysqli_connect("database", "root", "root", "php_tarefas");
-            mysqli_query($link, "INSERT INTO tarefas (titulo, concluida) 
-                                VALUES ('" . $titulo . "', " . $concluida . ")");
-
-            if (mysqli_error($link)) {
-                $error = true;
-                $message = "Houve um erro ao inserir a tarefa.";
-            } else {
-                $success = true;
-                $message = "Tarefa inserida com sucesso.";
-            }
-        }
-    } elseif ($_POST['action'] == 'excluir') {
-        $id = $_POST['id'];
-
-        $link = mysqli_connect("database", "root", "root", "php_tarefas");
-        mysqli_query ($link, "DELETE FROM tarefas WHERE id = {$id}");    
-        
-        if (mysqli_error($link)) {
-            $error = true;
-            $message = "Houve um erro ao excluir a tarefa.";
-        } else {                
-            $message = "Tarefa excluída com sucesso.";
-            $success = true;
-        }
-    } elseif ($_POST['action'] == 'done' || $_POST['action'] == 'undone') {
-        $id = $_POST['id'];
-        $action = $_POST['action'];
-        
-        $link = mysqli_connect("database", "root", "root", "php_tarefas");
-
-        if ($action == 'done') {
-            mysqli_query($link, "UPDATE tarefas SET concluida = '1' WHERE id = {$id}");
-        } elseif ($action == 'undone') {
-            mysqli_query($link, "UPDATE tarefas SET concluida = '0' WHERE id = {$id}");
-        }
-
-        if (mysqli_error($link)) {
-            $error = true;
-            $message = "Houve um erro ao alterar status da tarefa.";
-        } else {                
-            $message = "Status da tarefa alterado com sucesso.";
-            $success = true;
-        }
     }
-}
 
-function getTarefas() {
-    $link = mysqli_connect("database", "root", "root", "php_tarefas");
-    $result = mysqli_query($link, "SELECT * FROM tarefas");
-    $tarefas = mysqli_fetch_all($result, MYSQLI_ASSOC);
-    return $tarefas;
-}
+    function validarLancamento() 
+    {
+        if (!(isset($_POST['nome']) && strlen($_POST['nome']) > 0 && strlen($_POST['nome']) <= 250))
+            return false;
+        if (!(isset($_POST['tipo']) && strlen($_POST['tipo']) > 0 && strlen($_POST['tipo']) <= 250))
+            return false;
+        if (!(isset($_POST['valor'])) && is_numeric($_POST['valor']) && $_POST['valor'] !== 0)
+            return false;
+        if (!(isset($_POST['operacao']) && strlen($_POST['operacao']) == 1 ))
+            return false;
 
+        return true;
+    }
+
+    function setMessage ($type, $text) 
+    {
+        $message = [];
+        $message['type'] = $type;
+        $message['text'] = $text;
+
+        $_SESSION['message'] = $message;
+    }
+
+    function unsetMessage()
+    {
+        unset($_SESSION['message']);
+    }
+
+    function getMessage () 
+    {
+        return isset($_SESSION['message'])? $_SESSION['message'] : false;
+    }
+
+    function getLancamentos ()
+    {
+        global $conexao;
+
+        $sql = "SELECT * FROM lancamentos";
+        
+        $statement = $conexao->prepare($sql);
+
+        $statement->execute();
+
+        if($statement->rowCount() > 0) {
+            $lancamentos = $statement->fetchAll(PDO::FETCH_ASSOC);
+            return $lancamentos;
+        }
+
+        return [];
+    }
+
+    
 ?>
-<!DOCTYPE html>
-<html lang="pt-br">
 
+<!DOCTYPE html>
+<html lang="en">
 <head>
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de Tarefas</title>
+    <title>Controle Financeiro</title>
     <style>
         table {
-            border: 1px;
-            border-style: solid;
+            border: 1px solid black;
             border-collapse: collapse;
-            align-items: center;
         }
 
-        table th, tr td{
-            border: 1px;
-            border-style: solid;
-            text-align: center;
-        }
-
-        .aviso {
-            border: solid;
-            border-radius: 1em;
-            padding: 0em 0.5em;
-            margin: 0.5em;
-            color: white;
-        }
-
-        .success {
-            border-color: green;
-            background-color: green;
-        }
-
-        .error {
-            border-color: red;
-            background-color: red;
+        tr td {
+            border: 1px solid black;
         }
     </style>
 </head>
-
 <body>
-
+    <?php
+        $message = getMessage();
+        if($message) {
+            echo "<pre>";
+            var_dump($message);
+            echo "</pre>";
+            unsetMessage();
+        }
+    ?>
     <div>
-        <div <?= $success ? 'class="aviso success" style="display: block"' : 'style="display:none"'?> >
-                <p><?=$message?></p>
-        </div>
-        <div <?= $error ? 'class="aviso error" style="display: block"' : 'style="display:none"'?> >
-                <p><?=$message?></p>
-        </div>
-    </div>
-
-    <div>
-        <h2>Cadastro de tarefas</h2>
         <form action="index.php" method="POST">
             <div>
-                <label for="titulo">Titulo:</label><br>
-                <input type="text" name="titulo"  maxlength="255">
+                <label for="nome">Nome: </label>
+                <input type="text" name="nome" maxlength="250" required value="<?= isset($lancamento['nome'])?$lancamento['nome']:'' ?>">
             </div>
             <div>
-                <label for="">Concluída?</label><br>
-                <label for="true">Sim</label>
-                <input type="radio" name="concluida" value="true"><br>
-                <label for="false">Não</label>
-                <input type="radio" name="concluida" value="false" checked><br>
-
+                <label for="nome">Tipo: </label>
+                <input type="text" name="tipo" maxlength="250" required value="<?= isset($lancamento['tipo'])?$lancamento['tipo']:'' ?>">            
             </div>
             <div>
-                <input type="submit" value="inserir" name="">
+                <label for="valor">Valor: </label>
+                <input type="number" step="any" name="valor" minlength="0.1" maxlength="250" required value="<?= isset($lancamento['valor'])?$lancamento['valor']:'' ?>">
+            </div>
+            <label for="operacao">Valor: </label>
+            <select name="operacao" id="">
+                <option value="R" <?= !empty($lancamento['operacao']) && $lancamento['operacao'] == 'R'? 'selected': '' ?> >Receita</option>
+                <option value="D" <?= !empty($lancamento['operacao']) && $lancamento['operacao'] == 'D'? 'selected': '' ?> >Despesa</option>
+            </select>
+            <?php if($emEdicao) : ?>
+            <div>
+                <input type="submit" value="Atualizar">
+                <input type="hidden" name="action" value="atualizar">
+                <input type="hidden" name="id" value="<?= $lancamento['id'] ?> ">
+                <a href="index.php">Cancelar</a>
+            </div>
+            <?php else : ?>
+            <div>
+                <input type="submit" value="Inserir">
                 <input type="hidden" name="action" value="inserir">
             </div>
+            <?php endif; ?>
         </form>
     </div>
-    <br>
+        <?php  
+            $lancamentos = getLancamentos();
+            if(count($lancamentos) > 0) {
 
-    <div>
-        <?php
-            $tarefas = getTarefas();
-            if (count($tarefas) > 0) {
-                echo "<h2>Listagem de Tarefas</h2>";
+                echo "<div>";
                 echo "<table>";
-                    echo "<tr>";
-                        echo "<th>";
-                            echo "#ID";
-                        echo "</th>";
-                        echo "<th>";
-                            echo "Titulo";
-                        echo "</th>";
-                        echo "<th>";
-                            echo "Status";
-                        echo "</th>";
-                        echo "<th>";
-                            echo "Excluir";
-                        echo "</th>";
-                    echo "</tr>";
-                    foreach($tarefas as $tarefa) {
-                        echo "<tr>";
-                            echo "<td> {$tarefa['id']} </td>";
-                            echo "<td> {$tarefa['titulo']} </td>";
-                            if ($tarefa['concluida'] == 1) {
-                                echo '<td>
-                                        <form action="index.php" method="POST">
-                                            <input type="hidden" name="id" value="' . $tarefa['id'] . '">
-                                            <input type="hidden" name="action" value="undone">
-                                            <input type="submit" name="" value="🟢">
-                                        </form>
-                                    </td>';
-                            } else {
-                                echo '<td>
-                                        <form action="index.php" method="POST">
-                                            <input type="hidden" name="id" value="' . $tarefa['id'] . '">
-                                            <input type="hidden" name="action" value="done">
-                                            <input type="submit" name="" value="⭕">
-                                        </form>
-                                    </td>';
-                            }
-                            echo '<td>' .
-                                    '<form action="index.php" method="POST">
-                                        <input type="hidden" name="id" value="' . $tarefa['id'] . '">
-                                        <input type="hidden" name="action" value="excluir">
-                                        <input type="submit" name="" value="🗑️">
-                                    </form>' .
-                                '</td>';
-                        echo "</tr>";
-                    }
-                echo "</table>";
+                foreach ($lancamentos as $lancamento): ?>
+                    <tr>
+                        <td><?= $lancamento['id'] ?></td>
+                        <td><?= $lancamento['nome']?></td>
+                        <td><?= $lancamento['tipo']?></td>
+                        <td><?= $lancamento['valor']?></td>
+                        <td><?= $lancamento['operacao']?></td>
+                        <td>
+                            <form action="index.php" method="POST">
+                                <input type="hidden" name="id" value="<?= $lancamento['id'] ?>">
+                                <input type="submit" value="excluir" name="action">
+                            </form>
+                        </td>
+                        <td>
+                            <form action="index.php" method="POST">
+                                <input type="hidden" name="id" value="<?= $lancamento['id'] ?>">
+                                <input type="submit" value="editar" name="action">
+                            </form>
+                        </td>
+                        
+                    </tr>      
+                <?php endforeach;
+                echo "<table>";
+                echo "</div>";
             }
         ?>
     </div>
--- sql.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `tarefas` ( 
-  `id` INT AUTO_INCREMENT NOT NULL,
-  `titulo` VARCHAR(255) NOT NULL,
-  `concluida` TINYINT(1) NOT NULL,
-   PRIMARY KEY (`id`)
-);
